8136fa178f7d4bb42332ff4ba470cbac082a53b5
Nelly Rozova <igorevna.nailya@mail.ru>
2023-08-08 11:53:55 +0300

    lesson-6-2

--- routes/web.php
+++ routes/web.php
@@ -4,33 +4,31 @@ use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\IndexController as AdminController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 use App\Http\Controllers\NewsController;
-use App\Http\Controllers\WelcomePageController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', [WelcomePageController::class, 'index'])
-//    ->name('news.welcome');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-//Route::get('/hello/{name}', static function (string $name) {
-//    return "Hello, $name";
-//});
-//
-//Route::get('/AboutProject/', function () {
-//    return "Information about project";
-//});
-
-//admin
-Route::group(['prefix'=>'admin', 'as'=>'admin.'], static function () {
-    Route::get('/',AdminController::class)->name('index');
+//Admin
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], static function () {
+    Route::get('/', AdminController::class)->name('index');
     Route::resource('/categories', AdminCategoryController::class);
     Route::resource('/news', AdminNewsController::class);
 });
 
-//news
+//News
 Route::get('/news', [NewsController::class, 'index'])
     ->name('news.index');
 Route::get('/news/{id}', [NewsController::class, 'show'])
@@ -41,6 +39,10 @@ Route::get('/test', function() {
     return response()->download('robots.txt');
 });
 
-Route::get('/collection', function() {
-    return response()->download('robots.txt');
+Route::get('/collection', function () {
+    $array = [1,4,6,79,07,786,890,7788];
+    $collect = collect($array);
+    dd($collect->map(fn ($item) => $item * 2)->chunk(3)->sortBy(function ($item) {
+        return $item;
+    })->toArray());
 });
